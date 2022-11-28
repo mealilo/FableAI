0980bd51b402fbf29d1a49b57378e03b4136a245
mealilo <90289295+mealilo@users.noreply.github.com>
2022-11-28 14:09:31 -0700

    calling sample API working

---
+++ fableai/lib/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+
+import 'package:fableai/constants.dart';
+import 'package:fableai/user_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  Future<List<UserModel>?> getUsers() async {
+    try {
+      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        List<UserModel> _model = userModelFromJson(response.body);
+        return _model;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+}
--- fableai/lib/main.dart
+++ fableai/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:fableai/api_service.dart';
+import 'package:fableai/user_model.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -28,14 +31,15 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             backgroundColor: Colors.green,
             centerTitle: true,
-            title: const Text('FableAI2'),
+            title: const Text('FableAI'),
           ),
           // Calls the MyTextInput
-          body: MyTextInput()),
+          body: Home()),
     );
   }
 }
 
+// This calls the MyInput state, which does the actual logic, I think.
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -86,6 +90,69 @@ class MyTextInputState extends State<MyTextInput> {
   }
 }
 
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late List<UserModel>? _userModel = [];
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  void _getData() async {
+    _userModel = (await ApiService().getUsers())!;
+    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('REST API Example'),
+      ),
+      body: _userModel == null || _userModel!.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _userModel!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(_userModel![index].id.toString()),
+                          Text(_userModel![index].username),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(_userModel![index].email),
+                          Text(_userModel![index].website),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
+
+// default code for reference, we are not currently using this
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
