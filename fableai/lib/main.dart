5de10adc0042b339767271be91444658b1bebc75
user.name <user.email>
2022-12-06 13:57:08 -0700

    UI changes

--- fableai/lib/main.dart
+++ fableai/lib/main.dart
@@ -18,21 +18,17 @@ class MyApp extends StatelessWidget {
       title: 'FableAI',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blueGrey,
       ),
       home: Scaffold(
           appBar: AppBar(
-            backgroundColor: Colors.green,
+            backgroundColor: Color.fromARGB(255, 195, 195, 195),
+            foregroundColor: Colors.black,
             centerTitle: true,
-            title: const Text('FableAI'),
+            title: const Text(
+              'FableAI',
+              style: TextStyle(fontSize: 40),
+            ),
           ),
           // Calls the MyTextInput
           body: Home()),
@@ -72,58 +68,130 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: [
-      response == null
-          ? const Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Text(response!.choices![0]["text"].toString())),
-      const SizedBox(
-        height: 10,
-      ),
-      DropdownButton<String>(
-        value: dropdownValue,
-        icon: const Icon(Icons.arrow_downward),
-        elevation: 16,
-        style: const TextStyle(color: Colors.deepPurple),
-        underline: Container(
-          height: 2,
-          color: Colors.deepPurpleAccent,
-        ),
-        onChanged: (String? value) {
-          // This is called when the user selects an item.
-          setState(() {
-            dropdownValue = value!;
-          });
-        },
-        items: gpt3Models.map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
-      ),
-      const Text(
-        'Input Your Story Below:',
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      TextField(
-          keyboardType: TextInputType.multiline,
-          maxLines: null,
-          minLines: 4,
-          controller: myController,
-          decoration: InputDecoration(
-            hintText: "Enter your story here",
-            suffixIcon: IconButton(
-              icon: const Icon(Icons.send),
-              onPressed: () {
-                _getData(myController.text, dropdownValue);
-              },
+        body: CustomPaint(
+      painter: BackgroundPainter(),
+      child: Column(children: [
+        Row(children: <Widget>[
+          const Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Text(
+                'Choose your AI model:',
+                textAlign: TextAlign.right,
+              ),
             ),
           ),
-          onSubmitted: (String str) {
-            _getData(str, dropdownValue);
-          })
-    ]));
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_downward),
+            elevation: 16,
+            style: const TextStyle(color: Color.fromARGB(255, 72, 95, 105)),
+            underline: Container(
+              height: 2,
+              color: Colors.blueGrey,
+            ),
+            onChanged: (String? value) {
+              // This is called when the user selects an item.
+              setState(() {
+                dropdownValue = value!;
+              });
+            },
+            items: gpt3Models.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ]),
+        const Text(
+          'Input Your Story Below:',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: 16.0,
+              horizontal: ((MediaQuery.of(context).size.width) * 0.1)),
+          child: TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              minLines: 1,
+              controller: myController,
+              decoration: InputDecoration(
+                hintText: "Enter your story here",
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () {
+                    _getData(myController.text, dropdownValue);
+                  },
+                ),
+              ),
+              onSubmitted: (String str) {
+                _getData(str, dropdownValue);
+              }),
+        ),
+        response == null
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: ((MediaQuery.of(context).size.width) * 0.1)),
+                  child: Text(response!.choices![0]["text"].toString()),
+                )),
+      ]),
+    ));
   }
 }
+
+class BackgroundPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final height = size.height;
+    final width = size.width;
+    Paint paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
+    paint.color = Color.fromARGB(255, 255, 255, 255);
+    canvas.drawPath(mainBackground, paint);
+
+    Path ovalPath = Path();
+    // Start paint from 20% height to the left
+    ovalPath.moveTo(0, height * 0.2);
+    // paint a curve to the middle of the screen
+    ovalPath.quadraticBezierTo(
+        width * 0.45, height * 0.25, width * 0.51, height * 0.5);
+    // paint a curve to the bottom of screen
+    ovalPath.quadraticBezierTo(width * 0.58, height * 0.8, width * 0.1, height);
+    // draw remaining
+    ovalPath.lineTo(0, height);
+    // close line
+    ovalPath.close();
+
+    paint.color = Colors.grey.shade300;
+    canvas.drawPath(ovalPath, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+
+              // FutureBuilder<String?>(
+              //   future: getData(myController.text),
+              //   builder: (context, snapshot) {
+              //     if (snapshot.hasError) {
+              //       final error = snapshot.error;
+              //       return Text('$error');
+              //     } else if (snapshot.hasData) {
+              //       String data = snapshot.data!;
+              //       return Text(data);
+              //     } else {
+              //       return const CircularProgressIndicator();
+              //     }
+              //   }, //Testing
+
